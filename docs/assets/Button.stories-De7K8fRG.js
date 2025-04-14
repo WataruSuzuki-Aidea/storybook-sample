089888cb903d5dd919bdb4286b13073c5aff154c
鈴木亘 <wataru.suzuki@A-0116.local>
2025-04-14 16:54:45 +0900

    再ビルド

---
+++ docs/assets/Button.stories-De7K8fRG.js
@@ -0,0 +1,17 @@
+import{f as u}from"./index-BDOojamX.js";import{M as y}from"./Button-Cv6CX2nY.js";import"./vue.esm-bundler-DaV01QCu.js";const B={title:"Sample/Button",component:y,tags:["autodocs"],argTypes:{tone:{control:{type:"select"},options:["primary","secondary","tertiary"]},size:{control:{type:"select"},options:["small","medium","large"]}},args:{onClick:u()}},r={args:{primary:!0,tone:"tertiary",label:"ボタン"}},e={args:{tone:"secondary",label:"ボタン"}},a={args:{tone:"tertiary",label:"ボタン"}};var t,o,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    tone: "tertiary",
+    label: 'ボタン'
+  }
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var n,c,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    tone: 'secondary',
+    label: 'ボタン'
+  }
+}`,...(i=(c=e.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var m,p,l;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    tone: 'tertiary',
+    label: 'ボタン'
+  }
+}`,...(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const F=["Primary","Secondary","Tertiary"];export{r as Primary,e as Secondary,a as Tertiary,F as __namedExportsOrder,B as default};
